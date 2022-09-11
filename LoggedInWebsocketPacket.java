69c93f5d61d365493805aab7d0f9aa17215e7329
RoootTheFox <rooot@foxes4life.net>
2022-09-11 21:26:48 +0200

    mroe backend stuff woo

--- LoggedInWebsocketPacket.java
+++ LoggedInWebsocketPacket.java
@@ -2,9 +2,9 @@ package net.foxes4life.foxclient.networking.shared;
 
 public enum LoggedInWebsocketPacket {
     RESERVED((byte) 0), /* may be used in the future */
-    JOIN_SERVER((byte) 1),
-    LEAVE_SERVER((byte) 2),
-    GET_PLAYERS_ON_SERVER((byte) 3); // parameter: server address - response: list of sha1 hashes of uuids
+    JOIN_SERVER((byte) 1), // C2S
+    LEAVE_SERVER((byte) 2), // C2S
+    PLAYERS_ON_SERVER((byte) 3); // parameter: server address (C2S) - response: players on server (uuids, sha1) (S2C)
     LoggedInWebsocketPacket(byte id) {}
 
     public byte getId() {
